f859f31001d42b9885edfc50f0b9276ddaad00d3
Amer Qasrawi <amer.qasrawi@gmail.com>
2021-02-13 07:08:38 +0200

    update.

--- server/samiServer.js
+++ server/samiServer.js
@@ -80,7 +80,7 @@ app.use(multer({ dest: __dirname + '/Villas/' }).any());//added the .any, modifi
 let allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Credentials', true);
 
     
